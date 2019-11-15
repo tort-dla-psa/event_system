797689ba226609a08d9da932b513e9d7b7974950
tort_dla_psa <alexeev-eugene@outlook.com>
2019-11-15 14:00:47 +0300

    added memory example

---
+++ kinda_system/main.cpp
@@ -0,0 +1,188 @@
+#include <cmath>
+#include <thread>
+#include <iostream>
+#include <random>
+#include <iomanip>
+#include "ev_sys.h"
+
+using namespace ev_sys;
+
+class my_payload:public ev_sys::payload_data_holder{
+public:
+	enum type{
+		write,
+		read
+	}t;
+	size_t addr;
+	std::vector<uint8_t> data; 
+
+	my_payload(type t, size_t addr, std::vector<uint8_t> &&data)
+		:t(t),
+		addr(addr),
+		data(std::move(data))
+	{}
+};
+
+class output_filter{
+	std::mutex mt;
+public:
+	template<typename T>
+	void print(const std::vector<T> &data){
+		std::lock_guard<std::mutex> lk(mt);
+		for(const auto &val:data){
+			std::cout<<val<<" ";
+		}
+		std::cout<<"\n";
+	}
+
+	void print(const std::vector<uint8_t> &data){
+		std::lock_guard<std::mutex> lk(mt);
+		std::cout << std::hex << std::setfill('0');
+		for(const auto &val:data){
+			std::cout<<std::hex << std::setw(2)<<static_cast<int>(val)<<" ";
+		}
+		std::cout<<"\n";
+	}
+
+	void print(const std::string &data){
+		std::lock_guard<std::mutex> lk(mt);
+		std::cout<<data;
+	}
+};
+
+class unit:public module<unit>{
+	std::thread send_thr;
+	output_filter &fl;
+public:
+	target_port<unit> from_mem;
+	initiator_port<unit> to_mem;
+	std::queue<std::shared_ptr<my_payload>> data;
+
+	unit(const std::string &name,
+		output_filter &fl)
+		:module(name),
+		fl(fl),
+		to_mem("to_mem"),
+		from_mem("from_mem")
+	{
+		from_mem.set_func([this, &fl](std::unique_ptr<payload> &&pl){
+			auto data = std::dynamic_pointer_cast<my_payload>(pl->data);
+			fl.print("got data\n");
+			if(!data){
+				return;
+			}
+			fl.print(data->data);
+		});
+	}
+
+	~unit(){
+		if(send_thr.joinable()){
+			send_thr.join();
+		}
+	}
+
+	void start()override{
+		send_thr = std::thread([this](){
+			while(data.size() != 0){
+				auto pl = std::make_unique<payload>();
+				auto custom_pl = std::move(data.front());
+				pl->data = custom_pl;
+				to_mem.send(std::move(pl));
+				data.pop();
+				std::this_thread::sleep_for(std::chrono::milliseconds(1));
+			}
+		});
+		from_mem.start();
+	}
+};
+
+class memory:public module<memory>{
+	std::vector<uint8_t> mem;
+public:
+	std::vector<std::unique_ptr<target_port<memory>>> from_unit;
+	std::vector<std::unique_ptr<initiator_port<memory>>> to_unit;
+	output_filter &fl;
+
+	memory(const std::string &name, size_t bytes, size_t ports, output_filter &fl)
+		:module(name),
+		fl(fl)
+	{
+		to_unit.reserve(ports);
+		for(size_t i=0; i<ports; i++){
+			auto name = std::string("to_unit")+std::to_string(i);
+			to_unit.emplace_back(new initiator_port<memory>(std::move(name)));
+		}
+
+		from_unit.reserve(ports);
+		for(size_t i=0; i<ports; i++){
+			auto name = std::string("from_unit")+std::to_string(i);
+			from_unit.emplace_back(new target_port<memory>(std::move(name)));
+			from_unit.back()->set_func([this, &fl, i](std::unique_ptr<payload> &&pl){
+				auto pl_cast = std::dynamic_pointer_cast<my_payload>(pl->data);
+				if(!pl_cast){
+					return;
+				}
+				unsigned int addr = pl_cast->addr;
+				unsigned int len = pl_cast->data.size();
+				if(addr >= mem.size() || addr+len >= mem.size()){
+					throw_ex("reading outside of memory boundaries");
+				}
+				if(pl_cast->t == my_payload::read){
+					std::string str = "reading addr="+std::to_string(addr)+
+						" len="+std::to_string(len)+"\n";
+					fl.print(std::move(str));
+					std::copy(mem.begin()+addr, mem.begin()+addr+len, pl_cast->data.begin());
+					to_unit.at(i)->send(std::move(pl));
+				}else{
+					std::string str = "writing addr="+std::to_string(addr)+"\n";
+					fl.print(std::move(str));
+					std::copy(pl_cast->data.begin(), pl_cast->data.end(), mem.begin()+addr);
+				}
+			});
+		}
+
+		mem.resize(bytes);
+	}
+};
+
+std::queue<std::shared_ptr<my_payload>> gen_data(std::vector<std::shared_ptr<my_payload>> &&cmd){
+	std::queue<std::shared_ptr<my_payload>> q;
+	for(size_t i=0; i<cmd.size(); i++){
+		q.emplace(std::move(cmd[i]));
+	}
+	return std::move(q);
+}
+
+int main(){
+	output_filter f;
+	env e;
+	auto mem_size = std::pow(2,10);
+	auto mem = std::make_unique<memory>("ddr1", mem_size, 2,f);
+	auto u1 = std::make_unique<unit>("u1",f);
+	auto u2 = std::make_unique<unit>("u2",f);
+	{
+		std::vector<std::shared_ptr<my_payload>> cmds;
+		cmds.reserve(3);
+		cmds.emplace_back(new my_payload(my_payload::write, 0, {0x01, 0x02}));
+		cmds.emplace_back(new my_payload(my_payload::write, 2, {0x03, 0x04}));
+		cmds.emplace_back(new my_payload(my_payload::read, 0, std::move(std::vector<uint8_t>(4))));
+		u1->data = gen_data(std::move(cmds));
+	}
+	{
+		std::vector<std::shared_ptr<my_payload>> cmds;
+		cmds.reserve(3);
+		cmds.emplace_back(new my_payload(my_payload::write, 4, {0xFF, 0xFE, 0xFD, 0xFC}));
+		cmds.emplace_back(new my_payload(my_payload::write, 8, {0xFB, 0xFA, 0xEF, 0xEE}));
+		cmds.emplace_back(new my_payload(my_payload::read, 4, std::move(std::vector<uint8_t>(8))));
+		u2->data = gen_data(std::move(cmds));
+	}
+	e.tie(mem->to_unit[0], u1->from_mem);
+	e.tie(u1->to_mem, mem->from_unit[0]);
+	e.tie(mem->to_unit[1], u2->from_mem);
+	e.tie(u2->to_mem, mem->from_unit[1]);
+	e.add_module(std::move(mem));
+	e.add_module(std::move(u1));
+	e.add_module(std::move(u2));
+	e.start();
+	std::this_thread::sleep_for(std::chrono::milliseconds(50));
+}
